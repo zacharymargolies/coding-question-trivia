e1b275630d06c6e9577beef9a71b226a2d48e978
Zach Margolies <zachary.margolies@gmail.com>
2018-10-21 16:23:12 -0400

    fixed incorrect entry point

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import { AppLoading, Asset, Font, Icon } from "expo";
 import AppNavigator from "./navigation/AppNavigator";
-import { Provider } from "react-redux";
-import store from "./server/store";
 
 export default class App extends React.Component {
   state = {
