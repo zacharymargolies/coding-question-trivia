3392ea7998ecb3084185c6453225527a06bbe4f6
Zach Margolies <zachary.margolies@gmail.com>
2018-11-26 17:48:13 -0500

    update

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,13 +1,10 @@
 import { createSwitchNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
-import { TopicsStack } from './MainTabNavigator';
-import TopicsScreen from '../screens/TopicsScreen';
+import MainTabNavigator, { TopicsStack } from './MainTabNavigator';
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
-  // Topics: TopicsScreen
   Topics: TopicsStack
 });
