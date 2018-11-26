8e49ed748ba7d57d564ac4ede9f7517ba6fe680e
Zach Margolies <zachary.margolies@gmail.com>
2018-11-26 17:00:59 -0500

    information playground selector cards created and render

---
+++ components/PlaygroundCard.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { StyleSheet, Text, Image, View, TouchableOpacity } from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from 'react-native-responsive-screen';
+import { setCurrentTopic, fetchFactsByTopic } from '../server/store/fact';
+import { connect } from 'react-redux';
+
+class TopicCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { selector, navigate } = this.props;
+    return (
+      <TouchableOpacity
+        onPress={async () => {
+          // this.props.setCurrentTopic(topic.id);
+          // await this.props.getFactsByTopic(topic.id);
+          // navigate("Cards");
+        }}
+        style={styles.container}
+      >
+        // TOPIC TEXT
+        <Text
+          adjustsFontSizeToFit={true}
+          numberOfLines={1}
+          style={styles.topicText}
+        >
+          {selector.main}
+        </Text>
+        // TOPIC IMAGE
+        <Image style={styles.topicImage} source={{ uri: selector.image }} />
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: '#ffb142',
+    height: hp('20%'),
+    width: wp('30%'),
+    alignItems: 'center',
+    margin: wp('1.0%'),
+    borderRadius: wp('5%'),
+    shadowColor: '#cc8e35',
+    shadowOffset: {
+      width: 0,
+      height: 7
+    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+    elevation: 10
+  },
+  topicText: {
+    marginTop: hp('4%'),
+    fontSize: 26,
+    color: 'white'
+  },
+  topicImage: {
+    marginTop: hp('0.5%'),
+    height: hp('8%'),
+    width: hp('8%'),
+    borderRadius: hp('4%')
+  }
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentTopic: topicId => {
+    dispatch(setCurrentTopic(topicId));
+  },
+  getFactsByTopic: topicId => {
+    dispatch(fetchFactsByTopic(topicId));
+  }
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(TopicCard);
--- screens/InformationPlayground.js
+++ screens/InformationPlayground.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import PlaygroundCard from '../components/PlaygroundCard';
 import { connect } from 'react-redux';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from 'react-native-responsive-screen';
+import Axios from 'axios';
 
 class InformationPlayground extends React.Component {
   static navigationOptions = {
@@ -21,17 +23,44 @@ class InformationPlayground extends React.Component {
   }
 
   render() {
+    const allSelectors = [
+      {
+        main: 'Topic',
+        image: 'https://i.ytimg.com/vi/xOGxyw9DSa8/maxresdefault.jpg'
+      },
+      {
+        main: 'Difficulty',
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgF3kq4R9m6RqKS2W3weyEiBfVXaaTO8HmMAghHLH3yTXSe3tt'
+      },
+      {
+        main: 'Random',
+        image:
+          'https://cdn3.iconfinder.com/data/icons/flat-icons-web/40/Random-512.png'
+      }
+    ];
+    const { navigate } = this.props;
     return (
-      <React.Fragment>
-        <Text>Information Playground</Text>
-      </React.Fragment>
+      <View style={styles.container}>
+        {allSelectors.map(selector => (
+          <PlaygroundCard
+            selector={selector}
+            key={selector.id}
+            navigate={navigate}
+          />
+        ))}
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    backgroundColor: '#f7f1e3'
   }
 });
 
