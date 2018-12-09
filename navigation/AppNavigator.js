713c97f34f2c54d046dfda5b909731f12a37c5c8
Zach Margolies <zachary.margolies@gmail.com>
2018-12-09 16:41:42 -0500

    removed unnecessary code in navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,9 @@
 import { createSwitchNavigator } from 'react-navigation';
 
-import MainTabNavigator, { CardsStack } from './MainTabNavigator';
+import MainTabNavigator from './MainTabNavigator';
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator
-  // Cards: CardsStack
 });
